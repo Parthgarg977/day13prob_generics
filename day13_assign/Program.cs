32941b93b6314332fcf31d488179fc3381ba1661
parthgarg977 <parthgarg977@gmail.com>
2022-08-17 10:24:47 +0530

    UC1 1st commit

--- day13_assign/Program.cs
+++ day13_assign/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = true;
+            FindMax obj1 = new FindMax();
+            while (check)
+            {
+                Console.Write("Select Option:\n" +
+                    "1) Press 1 to find integer maximum number\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter 3 numbers");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        int num3 = Convert.ToInt32(Console.ReadLine());
+                        int result = obj1.FindMxInteger( num1, num2, num3);
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Maximum Number is:" + result);// To display Maximum Number
+                        Console.WriteLine("-----------------------------------");
+                        break;
+                    default:
+                        check = !check;
+                        break;
+                }
+            }
+        }
+    }
+}
