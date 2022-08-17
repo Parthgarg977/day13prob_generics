875063f1a004a8bdbb1ff39fcedc6521f8c6b3d2
parthgarg977 <parthgarg977@gmail.com>
2022-08-17 10:34:34 +0530

    UC2 1st commit

--- day13_assign/FindMax.cs
+++ day13_assign/FindMax.cs
@@ -14,5 +14,17 @@
             }
             return 0;
         }
+        public float FindMxInteger(float num1st, float num2nd, float num3rd)
+        {
+            if (num1st.CompareTo(num2nd) > 0 && num1st.CompareTo(num3rd) > 0)
+                return num1st;
+            if (num2nd.CompareTo(num1st) > 0 && num2nd.CompareTo(num3rd) > 0)
+                return num2nd;
+            if (num3rd.CompareTo(num1st) > 0 && num3rd.CompareTo(num2nd) > 0)
+            {
+                return num3rd;
+            }
+            return 0;
+        }
     }
 }
--- day13_assign/Program.cs
+++ day13_assign/Program.cs
@@ -11,7 +11,8 @@ namespace Generics
             while (check)
             {
                 Console.Write("Select Option:\n" +
-                    "1) Press 1 to find integer maximum number\n");
+                    "1) Press 1 to find integer maximum number\n"+
+                    "2) Press 2 to find float maximum number\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 
                 switch (option)
@@ -24,6 +25,16 @@ namespace Generics
                         int result = obj1.FindMxInteger( num1, num2, num3);
                         Console.WriteLine("-----------------------------------");
                         Console.WriteLine("Maximum Number is:" + result);// To display Maximum Number
+                        Console.WriteLine("-----------------------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter 3 numbers");
+                        float numf1 = float.Parse(Console.ReadLine());
+                        float numf2 = float.Parse(Console.ReadLine());
+                        float numf3 = float.Parse(Console.ReadLine());
+                        float resultf = obj1.FindMxInteger(numf1, numf2, numf3);
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Maximum Number is:" + resultf);// To display Maximum Number
                         Console.WriteLine("-----------------------------------");
                         break;
                     default:
